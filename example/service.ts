c2c10b0f0d0f5cb86aa314088a4057990d91a63d
wafuwafu13 <mariobaske@i.softbank.jp>
2022-09-08 18:15:36 +0900

    chore: Update example

--- example/service.ts
+++ example/service.ts
@@ -3,5 +3,18 @@ import { config, Mackerel } from "../deps.ts";
 const client = new Mackerel.Client(
   config({ path: "./example/.env" })["MACKELEL_API_KEY"],
 );
-const resp = await client.listServices();
-console.log(resp);
+
+const createService = await client.registerService({
+  name: "example-service",
+  memo: "this is example-service",
+});
+console.log(createService);
+// { name: "example-service", memo: "this is example-service", roles: [] }
+
+const listServices = await client.listServices();
+console.log(listServices);
+// [ { name: "example-service", memo: "this is example-service", roles: [] } ]
+
+const deleteService = await client.deleteService("example-service");
+console.log(deleteService);
+// { name: "example-service", memo: "this is example-service", roles: [] }
